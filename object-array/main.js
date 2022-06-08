0739160df2ad807a77d056b02ab8026faee2a25c
qweeq1 <qwertyyq369@gmail.com>
2022-06-08 16:45:57 +0300

    first task

--- object-array/main.js
+++ object-array/main.js
@@ -1,1 +1,14 @@
-console.log("Test")
+// Задача 1. Дан массив пользователей [{name: 'Vasya', age: 24}, {name: 'Petya', age: 12}, {name: 'Fedya', age: 6}, {name: 'Ann', age: 18}, {name: 'Nastya', age: 40} ]. Отсортируйте его по возрасту.
+// var arr =  [{name: 'Vasya', age: 24}, {name: 'Petya', age: 12}, {name: 'Fedya', age: 6}, {name: 'Ann', age: 18}, {name: 'Nastya', age: 40} ]
+// console.log(arr) // ??? уже отсортирован?
+// arr.sort(function (a,b){
+//     if(a.age > b.age){
+//         return 1;
+//     }
+//     if(a.age < b.age){
+//         return -1;
+//     }
+//     return 0
+// })
+//
+// console.log(arr)
