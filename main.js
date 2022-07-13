2b121d641c0bc310d8b69228f3de9adc4c627250
Kremez Roman <romaratmir@gmail.com>
2022-07-13 17:16:40 +0300

    forms

---
+++ main.js
@@ -0,0 +1,68 @@
+// var testForm = document.forms
+// console.log(testForm,'1')
+//
+//
+// var testForm2 = document.querySelector('form')
+// console.log(testForm2,'2')
+//
+//
+// document.body.insertAdjacentHTML(   // живая коллекция
+//     'beforeend',
+//     '<form action="" name="aaaaa"> </form>'
+// )
+//
+// Array.prototype.slice.call(testForm).forEach(function (elem){
+//     console.log(elem,'3')
+// })
+// ------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.text
+// console.log(elem)
+
+// -----------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.radio
+// console.log(elem)
+// -----------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.text
+// console.log(elem.form)
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.text
+// console.log(elem.value)
+// elem.value = 'asdf'
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.textarea
+// console.log(elem.value)
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.radio[1]
+// console.log(elem.checked)
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.checkbox[0]
+// console.log(elem.value)
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.file
+// console.log(elem.value)
+// --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.select
+// console.log(elem.options[elem.selectedIndex].text)
+// // --------------------------------------------
+// var tForm = document.forms.testForm
+// var elem = tForm.select
+// // elem.options[1].selected = true
+// console.log(elem.value)
+
+// Array.prototype.slice.call(elem.options).forEach(function (elem){
+//     if(elem.value === 100) elem.selected = true
+// })
+
+// var myOption = new Option('req', '10000',false,true) // добавление options в список
+// elem.append(myOption)
+
+
